1ec528e5441ea7ec6d55a7c021174893e25e3704
Nick Shin <nick.shin@phenixrts.com>
2025-02-22 13:43:11 -0600

    move common files to common locations
    * css
    * css-wb
    * js
    * html-wb

--- niknik-00-scrub-waybackmachine.sh
+++ niknik-00-scrub-waybackmachine.sh
@@ -3,6 +3,8 @@
 set -e
 #set -x
 
+# ==================================================
+
 scrubAllfiles() {
   find . -type f -name "*.css" -print | while read i; do
     echo $i
@@ -29,6 +31,34 @@ calculateMD5sums() {
   find . -type f -name "*.js" -exec md5sum '{}' \; | sort >niktmp-01-md5sums-js.txt
 }
 
+# --------------------------------------------------
+
+moveFiles() {
+  mkdir -p css css-wb fonts js images images-wb html-wb
+
+  # the following will refuse to copy over existing copy
+  #find . -path "*_files/*.html" -exec mv -t html-wb '{}' +
+  #find . -type f -name "css*.css" -exec mv -t css '{}' +
+  #find . -type f -name "*.css" -exec mv -t css-wb '{}' +
+  #find . -type f -name "*.js" -exec mv -t js '{}' +
+
+  # sledgehammer
+  find . -path "*_files/*.html" -print | while read i; do
+    mv "$i" html-wb/.
+  done
+  find . -path "*_files/css*.css" -print | while read i; do
+    mv "$i" css/.
+  done
+  find . -path "*_files/*.css" -print | while read i; do
+    mv "$i" css-wb/.
+  done
+  find . -path "*_files/*.js" -print | while read i; do
+    mv "$i" js/.
+  done
+}
+
 # ==================================================
-scrubAllfiles
-calculateMD5sums
+#scrubAllfiles
+#calculateMD5sums
+# --------------------
+moveFiles
