d4d8a2739eec0f0650bcfe9e3552c60b33c2b01f
Nick Shin <nick.shin@phenixrts.com>
2025-02-22 20:51:26 -0600

    remove duplicates and collect unique images

--- niknik-02-images.sh
+++ niknik-02-images.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-set -x
+#set -x
 
 # ==================================================
 
@@ -9,14 +9,34 @@ calculateMD5sums() {
   find . -type f \( -name "*.png" -or -name "*.jpg" \) -exec md5sum '{}' \; |
     sed -e 's/  /\t/g' |
     sed -e 's/\/\([^\/]\+$\)/\t\1/g' |
-    sort -t$'\t' -k3 >niktmp-03-images-md5sums.txt
+    sort -t$'\t' -k3 >niktmp-03a-images-md5sums.txt
 }
 
 scrubHtmlImages() {
-  find . -type f \( -name "*.png" -or -name "*.jpg" \) -exec file '{}' \; >niktmp-03-images-types.txt
-  grep HTML niktmp-03-images-types.txt | sed -e 's/: .\+//g' | xargs -I '{}' mv '{}' images-wb/.
+  find . -type f \( -name "*.png" -or -name "*.jpg" \) -exec file '{}' \; >niktmp-03a-images-types.txt
+  grep HTML niktmp-03a-images-types.txt | sed -e 's/: .\+//g' | xargs -I '{}' mv '{}' images-wb/.
 }
 
 # ==================================================
-calculateMD5sums
-scrubHtmlImages
+
+calculateMD5sumsLeftOverImages() {
+  echo "" >niktmp-03b-images-md5sums.txt
+  ls -1 */*_files/* | xargs -I '{}' md5sum '{}' |
+    sed -e 's/  /\t/g' |
+    sed -e 's/\/\([^\/]\+$\)/\t\1/g' |
+    sort -t$'\t' -k3 >niktmp-03b-images-md5sums.txt
+}
+
+moveImages() {
+  # the following will refuse to copy over existing copy
+  #mv */*_files/* images/.
+
+  # sledgehammer
+  ls -1 */*_files/* | xargs -I '{}' mv '{}' images/.
+}
+
+# ==================================================
+#calculateMD5sums
+#scrubHtmlImages
+calculateMD5sumsLeftOverImages
+moveImages
